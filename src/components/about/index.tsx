463f817bf2a001d93e229a9ac02bf5f173e4c174
Victor Kikuti <victor.kikuti@gmail.com>
2024-03-19 11:28:54 -0300

    fix(about) add responsity to mobile

--- src/components/about/index.tsx
+++ src/components/about/index.tsx
@@ -12,16 +12,16 @@ const About = () => {
   };
 
   return (
-    <div className="absolute w-full z-[-2] bg-white p-20">
+    <div className="absolute w-full z-[-2] bg-white py-20 md:px-4 px-6">
       <div className="grid md:grid-cols-2 grid-cols-1 lg:space-x-0 md:space-x-8 md:space-y-0 space-y-10">
-        <div className="flex md:justify-end justify-center items-center lg:pr-40 md:pr-10">
+        <div className="flex lg:justify-end justify-center items-center lg:pr-20">
           <img
             src="https://media.licdn.com/dms/image/C4D03AQGRH8cRF2jytA/profile-displayphoto-shrink_200_200/0/1649117623973?e=1716422400&v=beta&t=Y5Sf4VPuciAoeJ3SSjXy7oWuZqotvfo0i0OnPpSUA-w"
             alt=""
             className="md:w-96 md:h-96 w-80 h-80 rounded-full"
           />
         </div>
-        <div className="space-y-6 lg:w-4/5 md:w-11/12 flex flex-col justify-center">
+        <div className="space-y-6 lg:w-4/5 w-11/12 flex flex-col justify-center">
           <div className="lg:text-xl md:text-lg text-lg text-gray">
             Computer Science Student
           </div>
@@ -34,15 +34,15 @@ const About = () => {
             enjoy working in teams, as well as being proactive and curious to
             learn new technologies.
           </div>
-          <div className="font-bold hover:cursor-pointer space-x-4">
+          <div className="font-bold hover:cursor-pointer lg:space-x-4 md:space-x-2 space-x-1">
             <div
-              className="bg-light-blue inline-block px-6 py-3 rounded-md transition-transform transform hover:scale-105 hover:text-white"
+              className="bg-light-blue md:text-md text-sm inline-block lg:px-6 md:px-5 px-3 md:py-3 py-3 rounded-md transition-transform transform hover:scale-105 hover:text-white"
               onClick={() => handleDownload("https://drive.usercontent.google.com/u/0/uc?id=1WIQ_u-YvxuW56ItIA4m_AcmgPMUJM5qy&export=download")}
             >
               Download CV
             </div>
             <div
-              className="bg-light-blue inline-block px-6 py-3 rounded-md transition-transform transform hover:scale-105 hover:text-white"
+              className="bg-light-blue md:text-md text-sm inline-block lg:px-6 md:px-5 px-3 md:py-3 py-3 rounded-md transition-transform transform hover:scale-105 hover:text-white"
               onClick={() => handleDownload("https://drive.usercontent.google.com/u/0/uc?id=1tnHBffDHDy8zA1YZnYw1G6DlTLfs8bpz&export=download")}
             >
               Download CV(en)
