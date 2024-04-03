4d669957f32a5c7f6adc61f309f14080c6eece7b
Victor Kikuti <victor.kikuti@gmail.com>
2024-04-03 17:16:05 -0300

    fix(contact) add new margin

--- src/components/contact/index.tsx
+++ src/components/contact/index.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image'
 const Contact = () => {
   return (
     <div className="px-10 py-10 flex flex-row justify-center font-poppins">
-      <ul className="flex flex-row space-x-14 space-y-0 space-y-4 transition duration-300">
+      <ul className="flex flex-row space-x-14 transition duration-300">
         <li className="transition duration-300 hover:translate-y-[-3px] hover:cursor-pointer">
           <a href="https://www.linkedin.com/in/victorkmac/">
             <Image src="./linkedin.png" width={44} height={44} alt="Linkedin"/>
